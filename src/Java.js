a7a53c6a57674252dd84275fd1e1ae7205976264
mylla <camila.cerqueira@sga.pucminas.br>
2023-05-09 21:35:37 -0300

    feat: adicionado nome de um integrante - Camila. feat: adicionado título e descrição. feat: cabeçalho preenchido. feat: adicionado Orientador. feat: adcionado instruções de uso. feat: adicionado contexto feat: especificação do projeto. feat: retirada dos links úteis. feat: inserção das personas. feat: Inserção da história de usuários. feat: inserção de requisitos funcionais. feat:inserção dos não funcionais. feat: inserção das restrições. feat: inserção de wireframe feat: inserção de wireframe01 feat: inserção de páginas wireframes feat: descriçao dos wirefames 1,2,3. feat: descrição dos wireframes 4,5,6,7. feat: inserção dos templates. feat: inserção das descrições das páginas dos templates

---
+++ src/Java.js
@@ -0,0 +1,29 @@
+db = {
+    dados: [
+        {
+            titulo: 'Novas Receitas 1',
+            descricao: 'Bolo de Cenoura Vegano',
+            foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Plateful_of_Momo_in_Nepal.jpg/200px-Plateful_of_Momo_in_Nepal.jpg',
+        },
+        {
+            titulo: 'Novas Receitas 2',
+            descricao: 'Tabule',
+            foto: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Plateful_of_Momo_in_Nepal.jpg/200px-Plateful_of_Momo_in_Nepal.jpg',
+        },
+        {
+            titulo: 'Novas Receitas 3',
+            descricao: 'Pão sem Glútem',
+            foto: 'https://d1uz88p17r663j.cloudfront.net/original/f6b9eaa27f3b82bea01f3ac9fb447a8a_pao-aveia-sem-gluten-e-sem-lactose-receitas-nestle.jpg',
+        },
+        {
+            titulo: 'Novas Receitas 4',
+            descricao: 'Panqueca Americana sem glútem e sem lactose',
+            foto: 'https://media.istockphoto.com/id/516329534/photo/last-straw.jpg?s=612x612&w=is&k=20&c=Jcl89Ir2hsPviddHCps62G5lWNiuFwkyj-kaApnWwqo=',
+        },
+        {
+            titulo: 'Novas Receitas 5',
+            descricao: 'Bolo de Ceoura Vegano',
+            foto: 'https://i0.wp.com/presuntovegetariano.com.br/wp-content/uploads/2016/12/PanquecaDocePadraoFB.jpg?fit=1200%2C650&ssl=1',
+        }
+    ]
+}
